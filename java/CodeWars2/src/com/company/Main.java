958f19a99e0f1376eb6c276ce096f82d7c3280a1
Alexander Saavedra <Alexander@Alexanders-MBP.attlocal.net>
2018-09-18 20:45:31 -0700

    stuff

--- java/CodeWars2/src/com/company/Main.java
+++ java/CodeWars2/src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-
+        int var1 = 52;
+        int var2 = var1;
+        var1 = 127;
+        System.out.println("var1: " + var1 + " var2: " + var2);
     }
 }
+
