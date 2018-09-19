15a9f114b5c7f47fb1d351e1132d779e9ab1605c
Alexander Saavedra <Alexander@Alexanders-MBP.attlocal.net>
2018-09-18 20:39:25 -0700

    more basic classes

---
+++ java/CodeWars2/src/com/company/MyClass3.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class MyClass3
+{
+    private int a;
+
+    public MyClass3(int first)
+    {
+        this.a = first;
+    }
+}
---
+++ java/CodeWars2/src/com/company/Turtle.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Turtle {
+    // Other code here
+
+    public Turtle() {
+        // default constructor code here
+    }
+
+    // draw a line from current location to the location
+    // specified
+    public void drawLine(int x, int y) {
+        // more code
+    }
+}
