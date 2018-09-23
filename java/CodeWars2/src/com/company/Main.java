490d1f522273bf972db562a5481b6db308396c14
Alexander Saavedra <Alexander@Alexanders-MBP.attlocal.net>
2018-09-23 13:46:27 -0700

    general Java from UCSD OOP in Java class

--- java/CodeWars2/src/com/company/Main.java
+++ java/CodeWars2/src/com/company/Main.java
@@ -2,10 +2,7 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        int var1 = 52;
-        int var2 = var1;
-        var1 = 127;
-        System.out.println("var1: " + var1 + " var2: " + var2);
+
     }
 }
 
